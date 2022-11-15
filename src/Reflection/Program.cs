48f93301625a0996c8eff80a2f2252da8abc88d8
Uladlen Bondarau <Uladlen_Bondarau@epam.com>
2022-11-15 14:55:31 +0300

    Task 1 Initail commit

---
+++ src/Reflection/Program.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Reflection
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IConfiguration config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .AddEnvironmentVariables()
+                .Build();
+
+            var host = CreateHostBuilder(args).Build();
+            host.Services.GetRequiredService<Runner>().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            return Host.CreateDefaultBuilder(args)
+                .ConfigureServices(services =>
+                {
+                    services.AddTransient<IFileConfigurationProvider, FileConfigurationProvider>();
+                    services.AddTransient<IConfigurationManagerConfigurationProvider, ConfigurationManagerConfigurationProvider>();
+                    services.AddSingleton<IConfigurationComponentBase, ConfigurationComponentBase>();
+                    services.AddSingleton<Runner>();
+                });
+        }
+    }
+}
