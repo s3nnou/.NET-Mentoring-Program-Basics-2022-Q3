3e8639ad2700a32a838384a71fe6b68d000eb9f6
Uladlen Bondarau <Uladlen_Bondarau@epam.com>
2022-11-28 16:43:26 +0300

    Added FizzBuzz implementation

--- src/UnitTesting/FizzBuzz.cs
+++ src/UnitTesting/FizzBuzz.cs
@@ -13,12 +13,46 @@ namespace UnitTesting
     {
         public string PrintAll()
         {
-            throw new NotImplementedException();
+            var stringBuilder = new StringBuilder();
+
+            for (var i = 1; i <= 100; i++)
+            {
+                stringBuilder.AppendLine(PrintNumber(i));
+            }
+
+            return stringBuilder.ToString();
         }
 
         public string PrintNumber(int number)
         {
-            throw new NotImplementedException();
+            if (number <= 0 || number > 100)
+                throw new ArgumentException();
+
+            return DetermineFizzOrBuzz(number);
         }
+
+        private static string DetermineFizzOrBuzz(int number)
+        {
+            if (IsDivededByThree(number) && IsDivededByFive(number))
+            {
+                return "FizzBuzz";
+            }
+            else if (IsDivededByFive(number))
+            {
+                return "Buzz";
+            }
+            else if (IsDivededByThree(number))
+            {
+                return "Fizz";
+            }
+            else
+            {
+                return number.ToString();
+            }
+        }
+
+        private static bool IsDivededByThree(int number) => number % 3 == 0;
+
+        private static bool IsDivededByFive(int number) => number % 5 == 0;
     }
 }
