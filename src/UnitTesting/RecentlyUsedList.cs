7bcb79151590a1e680e52b7ac4ad88f9bd1032b4
Uladlen Bondarau <Uladlen_Bondarau@epam.com>
2022-11-28 16:40:35 +0300

    Initial commit
    Started RecentlyUsedList task and created tests

---
+++ src/UnitTesting/RecentlyUsedList.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+
+namespace UnitTesting
+{
+    public interface IRecentlyUsedList : IEnumerable<string>
+    {
+        void Add(string item);
+    }
+
+    public class RecentlyUsedList : IRecentlyUsedList
+    {        
+        private List<string> _items;
+
+        public int Count { get; private set; }
+
+        public int Size { get; private set; }
+
+        public string this[int index]
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public IEnumerator<string> GetEnumerator() => _items.GetEnumerator();
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
+        public void Add(string item)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ test/UnitTests.Tests/RecentlyUsedListTests.cs
@@ -0,0 +1,116 @@
+using FluentAssertions;
+using UnitTesting;
+
+namespace UnitTests.Tests
+{
+    public class RecentlyUsedListTests
+    {
+        [Test]
+        public void Indexer_WhenIndexIsInListCount_ShouldReturnItem()
+        {
+            // Arrange
+            var list = new RecentlyUsedList();
+            var item = "hello";
+            var index = 0;
+            var expectedItem = "hello";
+            list.Add(item);
+
+            // Act 
+            var recivedItem = list[index];
+
+            // Assert
+            recivedItem.Should().Be(expectedItem);
+        }
+
+        [Test]
+        public void Indexer_WhenIndexIsNegative_ShouldThrowException()
+        {
+            // Arrange
+            var list = new RecentlyUsedList();
+            var index = -1;
+
+            // Act 
+            var action = () => list[index];
+
+            // Assert
+            action.Should().Throw<IndexOutOfRangeException>();
+        }
+
+        [Test]
+        public void Indexer_WhenIndexIsLargerThenSize_ShouldThrowException()
+        {
+            // Arrange
+            var list = new RecentlyUsedList();
+            var index = 6;
+
+            // Act 
+            var action = () => list[index];
+
+            // Assert
+            action.Should().Throw<IndexOutOfRangeException>();
+        }
+
+        [Test]
+        public void Add_WhenNoDuplicatesExistsInList_ShouldAddItemToCollection()
+        {
+            // Arrange
+            var list = new RecentlyUsedList();
+            var item = "hello";
+            var expectedItem = "hello";
+            var expectedSize = 1;
+
+            // Act
+            list.Add(item);
+
+            // Assert
+            list[0].Should().BeEquivalentTo(expectedItem);
+            list.Count.Should().Be(expectedSize);
+        }
+
+        [Test]
+        public void Add_WhenDuplicatesExistsInList_ShouldThrowExceptionAndAbortAdding()
+        {
+            // Arrange
+            var list = new RecentlyUsedList();
+            var firstItem = "hello";
+            var secondItem = "hello";
+            var expectedItem = "hello";
+
+            // Act & Assert
+            list.Add(firstItem);
+            list[0].Should().BeEquivalentTo(expectedItem);
+            list.Count.Should().Be(1);
+            list.Add(secondItem);
+            list.Count.Should().Be(1);
+        }
+
+        [Test]
+        [TestCase("")]
+        [TestCase(null)]
+
+        public void Add_WhenDuplicatesExistsInList_ShouldThrowExceptionAndAbortAdding(string stringToAdd)
+        {
+            // Arrange
+            var list = new RecentlyUsedList();
+
+            // Act
+            var action = () => list.Add(stringToAdd);
+
+            // Assert
+            action.Should().Throw<ArgumentException>();
+        }
+
+        [Test]
+        public void RecentlyUsedList_WhenParametlessConsturctorUsed_ShouldUseDefaultSize()
+        {
+            // Arrange
+            var expectedLimit = 5;
+
+            // Act
+            var list = new RecentlyUsedList();
+
+            // Assert
+            list.Size.Should().Be(expectedLimit);
+        }
+    }
+}
