c65227c8436cf770fdd6b497691c7676476e2070
Uladlen Bondarau <Uladlen_Bondarau@epam.com>
2022-12-05 10:36:56 +0300

    Initial commit

---
+++ src/NetFundamentals.ClassLibrary/StringExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System.Text;
+
+namespace NetFundamentals.ClassLibrary
+{
+    public static class StringExtensions
+    {
+        public static string AddTimeStamp(this string text)
+        {
+            var builder = new StringBuilder();
+            builder.Append(DateTime.Now.ToShortTimeString());
+            builder.Append(' ');
+            builder.Append(text);
+            return builder.ToString();
+        }
+    }
+}
---
+++ src/NetFundamentals.WinFormsApp/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace NetFundamentals.WinFormsApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.nameTextBox = new System.Windows.Forms.TextBox();
+            this.acceptButton = new System.Windows.Forms.Button();
+            this.salutationTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dateCheckBox = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // nameTextBox
+            // 
+            this.nameTextBox.Location = new System.Drawing.Point(12, 24);
+            this.nameTextBox.Name = "nameTextBox";
+            this.nameTextBox.Size = new System.Drawing.Size(198, 23);
+            this.nameTextBox.TabIndex = 0;
+            // 
+            // acceptButton
+            // 
+            this.acceptButton.Location = new System.Drawing.Point(216, 24);
+            this.acceptButton.Name = "acceptButton";
+            this.acceptButton.Size = new System.Drawing.Size(75, 23);
+            this.acceptButton.TabIndex = 1;
+            this.acceptButton.Text = "Accept";
+            this.acceptButton.UseVisualStyleBackColor = true;
+            this.acceptButton.Click += new System.EventHandler(this.acceptButton_Click);
+            // 
+            // salutationTextBox
+            // 
+            this.salutationTextBox.Enabled = false;
+            this.salutationTextBox.Location = new System.Drawing.Point(12, 70);
+            this.salutationTextBox.Name = "salutationTextBox";
+            this.salutationTextBox.Size = new System.Drawing.Size(360, 23);
+            this.salutationTextBox.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 6);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(94, 15);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Enter your name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 50);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 15);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Salutation:";
+            // 
+            // dateCheckBox
+            // 
+            this.dateCheckBox.AutoSize = true;
+            this.dateCheckBox.Location = new System.Drawing.Point(297, 27);
+            this.dateCheckBox.Name = "dateCheckBox";
+            this.dateCheckBox.Size = new System.Drawing.Size(75, 19);
+            this.dateCheckBox.TabIndex = 5;
+            this.dateCheckBox.Text = "Add time";
+            this.dateCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(379, 121);
+            this.Controls.Add(this.dateCheckBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.salutationTextBox);
+            this.Controls.Add(this.acceptButton);
+            this.Controls.Add(this.nameTextBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private TextBox nameTextBox;
+        private Button acceptButton;
+        private TextBox salutationTextBox;
+        private Label label1;
+        private Label label2;
+        private CheckBox dateCheckBox;
+    }
+}
